794d140d4a1cec826db7c1f3c9c6f83c9722e44d
kurumi.m <kurumi0306maeda@gmail.com>
2023-07-01 08:05:41 +0000

    main.rb

---
+++ human.rb
@@ -0,0 +1,16 @@
+require './animal'
+require './thinkable'
+
+
+class Human < Animal
+  attr_accessor :hobby
+  
+  include Thinkable
+  
+  def initialize(name, age, hobby)
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+  end
+  
+end
